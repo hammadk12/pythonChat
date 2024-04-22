06161ef0cff5311298bf50ce05d4be6d93a39be8
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-22 11:00:31 -0400

    added further functionality

--- while.py
+++ while.py
@@ -7,20 +7,36 @@ contacts = [
 ]
 
 def main():
+    print("\n")
+    print('Contacts: \n')
     for contact in contacts:
         print(contact['name'], contact['location'], contact['age'], sep=", ")   
-    
+    print("\n")
+
     name_input = input("Enter contact name: ") 
 
     matched_name = [contact for contact in contacts if re.search(name_input, contact['name'], re.IGNORECASE)]
-    
+
+
     if matched_name:
         for contact in matched_name:
             deleteContact = input("Delete contact? (Y/N) ")
             if deleteContact.upper() == 'Y':
                 contacts.remove(contact)
                 print(f"Updated Contacts: {contacts}", sep="\n")
+                editContacts = input("Delete more contacts? (Y/N) ")
+                if editContacts.upper() == 'Y':
+                    main()
+                else:
+                    break
             elif deleteContact.upper() == 'N':
+                print("\n")
                 main()
-    
+            else:
+                print("Invalid input, please select (Y/N) ")
+    else:
+        print("Contact not found, try again.\n")
+        main()
+
 main()
+
