31a0d2b902611af8b4d3524dd9b50d559197cd51
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-24 08:26:14 -0400

    imported tkinter for GUI development

--- while.py
+++ while.py
@@ -1,4 +1,19 @@
 import re
+from tkinter import *
+
+root = Tk()
+
+e = Entry(root)
+e.pack()
+
+def myClick():
+    myLabel = Label(root, text="Clicked")
+    myLabel.pack()
+
+myButton = Button(root, text="Click me!", command=myClick)
+myButton.pack()
+
+root.mainloop()
 
 # original contacts list
 contacts = []
