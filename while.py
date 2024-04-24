d2528cc6f21f0c7c40bb48c9c5e88ab04445bba7
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-24 09:03:48 -0400

    added input widget

--- while.py
+++ while.py
@@ -3,14 +3,15 @@ from tkinter import *
 
 root = Tk()
 
-e = Entry(root)
+e = Entry(root, width=50)
 e.pack()
 
 def myClick():
-    myLabel = Label(root, text="Clicked")
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
     myLabel.pack()
 
-myButton = Button(root, text="Click me!", command=myClick)
+myButton = Button(root, text="Enter Name", command=myClick)
 myButton.pack()
 
 root.mainloop()
